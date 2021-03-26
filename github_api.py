5c4725a5fdb50033bd1aafbb28852d0101db0b93
uadson <uadsoncastro@gmail.com>
2021-03-26 19:56:23 -0300

    added file .flake8

--- github_api.py
+++ github_api.py
@@ -5,7 +5,7 @@ def search_avatar(user):
     """Busca o avatar de um usuário no GitHub
 
     Args:
-        user (str): string com o nome de usuário no GitHub
+        user (str): string com o nome de usuário no GitHub affasdfasfasdfasdfasdfasdfasdfasdfasdfasdfasdfasfasfdasdfasfd
         return: (str) string com o link do avatar
     """
 
@@ -13,5 +13,7 @@ def search_avatar(user):
     resp = requests.get(url)
     return resp.json()['avatar_url']
 
+
 if __name__ == '__main__':
     print(search_avatar('uadson'))
+# This is a new line that ends the file.
